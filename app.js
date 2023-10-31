46078e8f7a0907d5f046643460733802276079f6
nchfernandezc <ncfernandez@urbe.edu.ve>
2023-10-31 12:46:51 -0400

    actualización de opciones

--- app.js
+++ app.js
@@ -23,55 +23,73 @@ connection.connect((err) => {
     console.error('Error al conectar a la base de datos:', err);
     return;
   }
-  console.log('Conectado a la base de datos MySQL'); 
+  console.log('Conectado a la base de datos MySQL');
 });
 
-app.post('/register', async (req, res) => {
+app.post('/register', async (req, res, next) => {
+  try {
     const { name, username, password } = req.body;
-  
-    try {
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const sql = 'INSERT INTO user (name, username, password) VALUES (?, ?, ?)';
-      connection.query(sql, [name, username, hashedPassword], (err, result) => {
-        if (err) {
-          console.error('Error al registrar al usuario:', err);
-          //res.status(500).send('Error al registrar al usuario');
-        } else {
-          console.log('Usuario registrado');
-          res.status(200).send({ message: 'Registro completado' });
-        }
-      });
-    } catch (error) {
-      console.error('Error al registrar al usuario:', error);
-      res.status(500).send('Error al registrar al usuario');
-    }
-  });
-  
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const sql = 'INSERT INTO user (name, username, password) VALUES (?, ?, ?)';
+    connection.query(sql, [name, username, hashedPassword], (err, result) => {
+      if (err) {
+        console.error('Error al registrar al usuario:', err);
+        res.status(500).json({ error: 'Error al registrar al usuario. Por favor, inténtelo de nuevo más tarde.' });
+      } else {
+        console.log('Usuario registrado');
+        res.status(200).json({ message: 'Registro completado' });
+      }
+    });
+  } catch (error) {
+    console.error('Error al registrar al usuario:', error);
+    res.status(500).json({ error: 'Error al registrar al usuario. Por favor, inténtelo de nuevo más tarde.' });
+  }
+});
 
-  app.post('/authenticate', async (req, res) => {
-    const { username, password } = req.body;
-  
+app.post('/authenticate', async (req, res, next) => {
     try {
+      const { username, password } = req.body;
       const sql = 'SELECT * FROM user WHERE username = ?';
       connection.query(sql, [username], async (err, results) => {
         if (err) {
-          return res.sendStatus(500); 
+          return res.status(500).json({ error: 'Error en la autenticación del usuario. Por favor, inténtelo de nuevo más tarde.' });
         }
         if (results.length === 0) {
-          return res.sendStatus(404); 
+          return res.status(404).json({ error: 'Usuario no encontrado.' });
         }
   
         const user = results[0];
         const isPasswordCorrect = await bcrypt.compare(password, user.password);
         if (isPasswordCorrect) {
-          return res.sendStatus(200);
+            console.log("usuario autenticado");
+          // No envíes el mensaje de autenticación exitosa si no quieres mostrarlo al usuario
+          return res.status(200).end();
         } else {
-          return res.sendStatus(401); 
+          return res.status(401).json({ error: 'Credenciales incorrectas. Por favor, inténtelo de nuevo.' });
         }
       });
     } catch (error) {
       console.error('Error:', error);
-      return res.sendStatus(500); 
+      return res.status(500).json({ error: 'Error en la autenticación del usuario. Por favor, inténtelo de nuevo más tarde.' });
+    }
+  });
+  
+  app.post('/submit_request', (req, res) => {
+    try {
+      const { id, fecha, tipo_de_requerimiento, descripcion, nota } = req.body;
+      const sql = 'INSERT INTO solicitud (id, fecha, tipo_de_requerimiento, descripcion, nota) VALUES (?, ?, ?, ?, ?)';
+      connection.query(sql, [id, fecha, tipo_de_requerimiento, descripcion, nota], (err, result) => {
+        if (err) {
+          console.error('Error al insertar los datos en la tabla de solicitud:', err);
+          res.status(500).json({ error: 'Error al enviar el formulario de requerimientos. Por favor, inténtelo de nuevo más tarde.' });
+        } else {
+          console.log('Datos insertados correctamente en la tabla de solicitud');
+          res.status(200).json({ message: 'El formulario de requerimientos se ha enviado correctamente' });
+        }
+      });
+    } catch (error) {
+      console.error('Error al manejar el formulario de requerimientos:', error);
+      res.status(500).json({ error: 'Error al manejar el formulario de requerimientos. Por favor, inténtelo de nuevo más tarde.' });
     }
   });
   
@@ -79,5 +97,3 @@ app.post('/register', async (req, res) => {
 app.listen(3000, () => {
   console.log('Servidor iniciado en el puerto 3000...');
 });
-
-module.exports = app;
